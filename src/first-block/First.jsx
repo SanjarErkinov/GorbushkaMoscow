d7abdfcc84f58db622e12685f4d68282cec19eea
SanjarErkinov <hdrize8@gmail.com>
2023-04-27 23:11:14 +0300

    first commit

---
+++ src/first-block/First.jsx
@@ -0,0 +1,94 @@
+import styles from './First.module.css'
+import Image from 'next/image';
+
+const First = () => {
+    return (
+        <div className={styles.first}>
+            <div className={styles.first__container}>
+                <div className={styles.first__body}>
+                    <div className={styles.banner__block}>
+                        <div className={styles.banner__slide}>
+                            <div className={styles.banner__slide_content}>
+                                <h1>Большой</h1>
+                                <h1>и еще больше</h1>
+                                <p>Iphone 14 pro и Iphone 14 <br /> pro Max уже в продаже. </p>
+                                <button>Купить</button>
+                            </div>
+                        </div>
+                        <div className={styles.banner__product}>
+                            <div className={styles.banner__product_container}>
+                                <div className={styles.banner__product_body}>
+                                    <div className={styles.banner__product_title}>
+                                        <h1>Товар дня</h1>
+                                        <div className={styles.card__icon}>
+                                            <Image src='/Image/FirstBlock/Union.png' alt='Union' width={14} height={17} />
+                                            <div className={styles.product__favorites}></div>
+                                        </div>
+                                    </div>
+                                    <div className={styles.banner__product_slide}>
+                                        <Image src='/Image/FirstBlock/apple-watch.png' alt="Apple-Watch" width={128} height={151} />
+                                    </div>
+                                    <div className={styles.banner__product_info}>
+                                        <p>Умные часы Apple Watch SE 40 мм <br /> Aluminium Case, серебристый/синий омут</p>
+                                        <Image src='/Image/FirstBlock/reviews.png' alt="Reviews" width={73} height={12} />
+                                        <div className={styles.product_info_subtitle}>
+                                            <div>
+                                                <p>33 990 ₽</p>
+                                                <h2>31 990 ₽</h2>
+                                            </div>
+                                            <div>
+                                                <button>Купить</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.catalog__block}>
+                        <div className={styles.catalog__card_items}>
+                            <div className={styles.catalog__card}>
+                                <p>Смартфоны</p>
+                                <div>
+                                    <Image src='/Image/FirstBlock/catalog-smartfon.png' alt="Smartfon" width={123} height={135} />
+                                </div>
+                            </div>
+                            <div className={styles.catalog__card}>
+                                <p>Смартфоны</p>
+                                <div>
+                                    <Image src='/Image/FirstBlock/catalog-smartfon.png' alt="Smartfon" width={123} height={135} />
+                                </div>
+                            </div>
+                            <div className={styles.catalog__card}>
+                                <p>Смартфоны</p>
+                                <div>
+                                    <Image src='/Image/FirstBlock/catalog-smartfon.png' alt="Smartfon" width={123} height={135} />
+                                </div>
+                            </div>
+                            <div className={styles.catalog__card}>
+                                <p>Смартфоны</p>
+                                <div>
+                                    <Image src='/Image/FirstBlock/catalog-smartfon.png' alt="Smartfon" width={123} height={135} />
+                                </div>
+                            </div>
+                            <div className={styles.catalog__card}>
+                                <p>Смартфоны</p>
+                                <div>
+                                    <Image src='/Image/FirstBlock/catalog-smartfon.png' alt="Smartfon" width={123} height={135} />
+                                </div>
+                            </div>
+                            <div className={styles.catalog__card}>
+                                <p>Смартфоны</p>
+                                <div>
+                                    <Image src='/Image/FirstBlock/catalog-smartfon.png' alt="Smartfon" width={123} height={135} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default First;
